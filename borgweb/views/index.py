6434e65ece264fcae52b1642ca576eb70cdc37db
Thomas Waldmann <tw@waldmann-edv.de>
2015-06-15 15:29:22 +0200

    index view shows logfile

--- borgweb/views/index.py
+++ borgweb/views/index.py
@@ -1,8 +1,19 @@
-from flask import render_template
+import os
+
+from flask import current_app, render_template
 
 from . import blueprint
 
 
 @blueprint.route('/')
 def index():
-    return render_template('index.html')
+    log_dir = current_app.config['LOG_DIR']
+    log_files = os.listdir(log_dir)
+    log_file = log_files[0] if log_files else None
+    if log_file:
+        log_file = os.path.join(log_dir, log_file)
+        with open(log_file, 'r') as f:
+            log_content = f.read()
+    else:
+        log_content = 'No log available.'
+    return render_template('index.html', log_content=log_content)
