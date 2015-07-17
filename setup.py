6de049b9f373b9f7995cd625008e21a719506b28
Thomas Waldmann <tw@waldmann-edv.de>
2015-07-18 00:34:13 +0200

    flask requires python 3.3+

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: System :: Archiving :: Backup',
